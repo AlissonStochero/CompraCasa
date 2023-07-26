5f8616d82a314061a8b7f2a3b11416f2d7bff487
AlissonStochero <alissonstochero@gmail.com>
2023-07-26 19:42:49 -0300

    first

---
+++ App.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("ok");
+        }
+    }
+}
